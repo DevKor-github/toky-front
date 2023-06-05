ce76708b7ec503515d0f7dbc6a4c2b4b96c99deb
KUcodemaster <jh9485@korea.ac.kr>
2023-06-05 13:13:14 +0900

    Feat: 베팅 페이지 UI 개발

--- toky-front/src/components/bets/BetTopBar.tsx
+++ toky-front/src/components/bets/BetTopBar.tsx
@@ -5,41 +5,47 @@ import { styled } from "styled-components";
 import LeftArrow from "../../../public/image/leftArrow.webp";
 import Image from "next/image";
 //import "./BetTopBar.css"
-export default function BetTopBar() {
-    const router = useRouter();
-    const handleBack = () => {
-        router.back();
-    };
 
-    return (
-        <TopBarWrapper>
-            <Image
-                style={{ position: "absolute", left: "16px", bottom: "14px" }}
-                onClick={handleBack}
-                src={LeftArrow}
-                alt="back"
-                width={20}
-            />
-            <Title>
-                <h4>2023 정기전 승부예측</h4>
-            </Title>
-        </TopBarWrapper>
-    );
+export default function BetTopBar() {
+  const router = useRouter();
+  const handleBack = () => {
+    router.push("/");
+  };
+
+  return (
+    <TopBarWrapper>
+      <Image
+        style={{
+          position: "absolute",
+          left: "16px",
+          bottom: "14px",
+          cursor: "pointer",
+        }}
+        onClick={handleBack}
+        src={LeftArrow}
+        alt="back"
+        width={20}
+      />
+      <Title>
+        <h4>2023 정기전 승부예측</h4>
+      </Title>
+    </TopBarWrapper>
+  );
 }
 
 const TopBarWrapper = styled.div`
-    width: 100%;
-    height: 100px;
-    position: relative;
+  width: 100%;
+  height: 100px;
+  position: relative;
 `;
 const Title = styled.div`
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%);
-    bottom: 14px;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  bottom: 14px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
 `;
 const H4 = styled.h4``;
 
