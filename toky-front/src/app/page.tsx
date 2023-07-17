c2c4f123f70b85a339695d42657c5de350f7d4dd
overthestream <njhnjh02@naver.com>
2023-07-17 21:22:47 +0900

    refactor

--- toky-front/src/app/page.tsx
+++ toky-front/src/app/page.tsx
@@ -4,17 +4,16 @@ import MainInfo from "@/components/home/MainInfo";
 import MainFooter from "@/components/home/MainFooter";
 import MainTopBar from "@/components/home/MainTopBar";
 import PointInfo from "@/components/home/PointInfo";
-import styled from "styled-components";
 
 export default function Home() {
-  return (
-    <div>
-      <MainTopBar />
-      <MainInfo />
-      <BetInfo />
-      <PointInfo />
-      <DrawInfo />
-      <MainFooter />
-    </div>
-  );
+	return (
+		<div>
+			<MainTopBar />
+			<MainInfo />
+			<BetInfo />
+			<PointInfo />
+			<DrawInfo />
+			<MainFooter />
+		</div>
+	);
 }
--- toky-front/src/components/home/MainTopBar.tsx
+++ toky-front/src/components/home/MainTopBar.tsx
@@ -1,75 +1,74 @@
 "use client";
 import { styled } from "styled-components";
 import Link from "next/link";
-import Logo from "../../../public/image/TokyMainLogo.png";
+import Logo from "../../../public/image/logo.svg";
 import Image from "next/image";
 import { easeInOut, motion } from "framer-motion";
 import MainArrow from "../../../public/image/MainArrow.svg";
 
 export default function MainTopBar() {
-  return (
-    //<MainTopBarWrapper>
-    <MainTopBarWrapper
-      className="MainTopBarWrapper"
-      initial={{
-        opacity: 0,
-        y: -90,
-      }}
-      animate={{
-        opacity: 1,
-        y: 0,
-      }}
-      transition={{ duration: 1.2, ease: easeInOut }}
-    >
-      <Image
-        src={Logo}
-        alt="logo"
-        width={89}
-        style={{
-          position: "absolute",
-          left: "5%",
-          top: "50%",
-          transform: "translate(0, -50%)",
-        }}
-      />
-      <Link
-        href="/bets"
-        style={{
-          position: "absolute",
-          right: "5%",
-          top: "50%",
-          transform: "translate(0%, -50%)",
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        <h4
-          style={{
-            fontFamily: "Spoqa Han Sans Neo",
-            fontStyle: "normal",
-            fontWeight: "400",
-            fontSize: "16px",
-            lineHeight: "20px",
-            letterSpacing: "-0.06em",
-            marginRight: "10px",
-          }}
-        >
-          바로가기
-        </h4>
-        <Image src={MainArrow} alt="main arrow" width={9} height={14} />
-      </Link>
-    </MainTopBarWrapper>
-    //</MainTopBarWrapper>
-  );
+	return (
+		<MainTopBarWrapper
+			className="MainTopBarWrapper"
+			initial={{
+				opacity: 0,
+				y: -90,
+			}}
+			animate={{
+				opacity: 1,
+				y: 0,
+			}}
+			transition={{ duration: 1.2, ease: easeInOut }}
+		>
+			<Image
+				src={Logo}
+				alt="logo"
+				width={89}
+				style={{
+					position: "absolute",
+					left: "5%",
+					top: "50%",
+					transform: "translate(0, -50%)",
+				}}
+			/>
+			<Link
+				href="/bets"
+				style={{
+					position: "absolute",
+					right: "5%",
+					top: "50%",
+					transform: "translate(0%, -50%)",
+					display: "flex",
+					alignItems: "center",
+				}}
+			>
+				<h4
+					style={{
+						fontFamily: "Spoqa Han Sans Neo",
+						fontStyle: "normal",
+						fontWeight: "400",
+						fontSize: "16px",
+						lineHeight: "20px",
+						letterSpacing: "-0.06em",
+						marginRight: "10px",
+					}}
+				>
+					바로가기
+				</h4>
+				<Image src={MainArrow} alt="main arrow" width={9} height={14} />
+			</Link>
+		</MainTopBarWrapper>
+		//</MainTopBarWrapper>
+	);
 }
 
 const MainTopBarWrapper = styled(motion.div)`
-  position: fixed;
-  color: white;
-  min-width: 375px;
-  width: 100%;
-  height: 58px;
-  background: #121212;
-  box-shadow: 0px 4px 4px rgba(18, 18, 18, 0.25);
-  z-index: 3;
+	position: fixed;
+	color: white;
+	min-width: 375px;
+	width: 100%;
+	height: 58px;
+	background: #121212;
+	box-shadow: 0px 4px 4px rgba(18, 18, 18, 0.25);
+	z-index: 3;
 `;
