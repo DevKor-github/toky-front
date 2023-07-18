395415d3f367efe873f3fac0057ef1174213c309
SeokWon <tjrdnjs1534@naver.com>
2023-07-18 10:43:52 +0900

    fix: link 틀어진거 수정

--- toky-front/src/components/common/NavigationBar.tsx
+++ toky-front/src/components/common/NavigationBar.tsx
@@ -1,70 +1,74 @@
-'use client';
+"use client";
 
-import { usePathname, useRouter } from 'next/navigation';
-import styled from 'styled-components';
-import Image from 'next/image';
-import Logo from '../../../public/image/logo.svg';
-import SideBar from './SideBar';
+import { usePathname, useRouter } from "next/navigation";
+import styled from "styled-components";
+import Image from "next/image";
+import Logo from "../../../public/image/logo.svg";
+import SideBar from "./SideBar";
 
 export default function NavigationBar() {
-	const pathname = usePathname();
-	const router = useRouter();
+  const pathname = usePathname();
+  const router = useRouter();
 
-	return (
-		<Wrapper>
-			<Image src={Logo} alt='logo' />
-			<NavigationWrapper>
-				<div style={{ display: 'flex', gap: 23 }}>
-					<NavigationItem
-						selected={pathname === '/analyze'}
-						onClick={() => {
-							router.push('/analyze');
-						}}
-					>
-						전력분석
-					</NavigationItem>
-					<NavigationItem selected={pathname === '/bets'} onClick={() => router.push('/bets')}>
-						승부예측
-					</NavigationItem>
-					<NavigationItem
-						selected={pathname === '/ranking'}
-						onClick={() => router.push('/ranking')}
-					>
-						랭킹
-					</NavigationItem>
-				</div>
-				<SideBar />
-			</NavigationWrapper>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <Image src={Logo} alt="logo" />
+      <NavigationWrapper>
+        <div style={{ display: "flex", gap: 23 }}>
+          <NavigationItem
+            selected={pathname === "/analysis"}
+            onClick={() => {
+              router.push("/analysis");
+            }}
+          >
+            전력분석
+          </NavigationItem>
+          <NavigationItem
+            selected={pathname === "/bets"}
+            onClick={() => router.push("/bets")}
+          >
+            승부예측
+          </NavigationItem>
+          <NavigationItem
+            selected={pathname === "/ranking"}
+            onClick={() => router.push("/ranking")}
+          >
+            랭킹
+          </NavigationItem>
+        </div>
+        <SideBar />
+      </NavigationWrapper>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-	position: absolute;
-	top: 0px;
-	width: 100%;
-	height: 46px;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	padding: 0px 23px;
-	background-color: #121212;
-	border-bottom: 1px solid rgba(255, 255, 255, 0.15);
-	z-index: 10;
+  position: absolute;
+  top: 0px;
+  width: 100%;
+  height: 46px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 23px;
+  background-color: #121212;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.15);
+  z-index: 10;
 `;
 
 const NavigationWrapper = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 25px;
+  display: flex;
+  align-items: center;
+  gap: 25px;
 `;
 
 const NavigationItem = styled.div<{ selected: boolean }>`
-	display: flex;
-	align-items: center;
-	height: 46px;
-	font-weight: 500;
-	color: rgba(255, 255, 255, 0.38);
+  display: flex;
+  align-items: center;
+  height: 46px;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 0.38);
 
-	${({ selected }) => selected && 'color: #ffffff; border-bottom: 3px solid #ffffff;'}
+  ${({ selected }) =>
+    selected && "color: #ffffff; border-bottom: 3px solid #ffffff;"}
 `;
