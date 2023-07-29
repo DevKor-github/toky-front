fa87d3725de47d91550147df8027cb8ef5bb32aa
yunz0926 <chldbswl9@gmail.com>
2023-07-29 20:17:54 +0900

    feat: 포인트 정책

--- toky-front/src/app/draw/page.tsx
+++ toky-front/src/app/draw/page.tsx
@@ -1,24 +1,16 @@
-'use client';
-import styled from 'styled-components';
-import ResponsiveBox from '@/components/common/ResponsiveBox';
-import TopBar from '@/components/common/TopBar';
-import UserPoint from '@/components/draw/UserPoint';
-import DrawGift from '@/components/draw/DrawGift';
+"use client";
+import TopBar from "@/components/common/TopBar";
+import UserPoint from "@/components/draw/UserPoint";
+import DrawGift from "@/components/draw/DrawGift";
+import Policy from "@/components/draw/Policy";
 
 export default function Draw() {
-	return (
-		<Wrapper>
-			<TopBar title='내 포인트' />
-			<UserPoint />
-			<DrawGift />
-		</Wrapper>
-	);
+  return (
+    <>
+      <TopBar title="내 포인트" />
+      <UserPoint />
+      <DrawGift />
+      <Policy />
+    </>
+  );
 }
-
-const Wrapper = styled(ResponsiveBox)`
-	position: absolute;
-	background-color: #222222;
-	right: 0;
-	transition: 1s ease;
-	padding: 0px;
-`;
