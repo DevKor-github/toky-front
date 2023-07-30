b31a9809baf131dec58485c20986b55e68b0bb6b
SeokWon <tjrdnjs1534@naver.com>
2023-07-30 22:17:53 +0900

    feat: AuthContext 적용

--- toky-front/src/lib/httpClient.ts
+++ toky-front/src/lib/httpClient.ts
@@ -1,77 +1,80 @@
+import AuthContext from "@/components/common/AuthContext";
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 export const refresh = async () => {
-	const refreshToken = localStorage.getItem("refreshToken");
+  const refreshToken = localStorage.getItem("refreshToken");
 
-	try {
-		const res = await axios.post(
-			`${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
-			{},
-			{ headers: { Authorization: `Bearer ${refreshToken}` } }
-		);
-		if (res.data.accessToken && res.data.refreshToken) {
-			localStorage.setItem("accessToken", res.data.accessToken);
-			localStorage.setItem("refreshToken", res.data.refreshToken);
-			return true;
-		}
-	} catch (err) {
-		console.log(err);
-	}
-	localStorage.removeItem("refreshToken");
-	localStorage.removeItem("accessToken");
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
+      {},
+      { headers: { Authorization: `Bearer ${refreshToken}` } }
+    );
+    if (res.data.accessToken && res.data.refreshToken) {
+      localStorage.setItem("accessToken", res.data.accessToken);
+      localStorage.setItem("refreshToken", res.data.refreshToken);
+      return true;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  localStorage.removeItem("refreshToken");
+  localStorage.removeItem("accessToken");
 
-	return false;
+  return false;
 };
 
 export const setTokenFromCookie = () => {
-	let accessToken = null,
-		refreshToken = null;
+  let accessToken = null,
+    refreshToken = null;
 
-	document.cookie.split(";").forEach((c) => {
-		if (c.trim().startsWith("access-token=")) accessToken = c.split("=")[1];
-		if (c.trim().startsWith("refresh-token=")) refreshToken = c.split("=")[1];
-	});
-	if (!accessToken || !refreshToken) {
-		localStorage.removeItem("refreshToken");
-		return false;
-	}
+  document.cookie.split(";").forEach((c) => {
+    if (c.trim().startsWith("access-token=")) accessToken = c.split("=")[1];
+    if (c.trim().startsWith("refresh-token=")) refreshToken = c.split("=")[1];
+  });
+  if (!accessToken || !refreshToken) {
+    localStorage.removeItem("refreshToken");
+    return false;
+  }
 
-	document.cookie = "access-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-	document.cookie = "refresh-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie =
+    "access-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  document.cookie =
+    "refresh-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-	localStorage.setItem("accessToken", accessToken);
-	localStorage.setItem("refreshToken", refreshToken);
-
-	return true;
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("refreshToken", refreshToken);
 };
 
 const client = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 client.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-	const token = localStorage.getItem("accessToken");
-	if (token && config.headers) {
-		config.headers["Authorization"] = `Bearer ${token}`;
-	}
-	return config;
+  const token = localStorage.getItem("accessToken");
+  if (token && config.headers) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
 });
 
 client.interceptors.response.use(
-	(response) => response,
-	async (error) => {
-		console.log("error", error);
-		if (error.response && error.response.status === 401) {
-			localStorage.removeItem("accessToken");
-			const refreshSuccess = await refresh();
-			if (refreshSuccess) {
-				const originalRequest = error.config;
-				originalRequest.headers.Authorization = `Bearer ${localStorage.getItem("accessToken")}`;
-				return client.request(originalRequest);
-			}
-			window.location.href = "/login";
-		} else return error.response;
-	}
+  (response) => response,
+  async (error) => {
+    console.log("error", error);
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("accessToken");
+      const refreshSuccess = await refresh();
+      if (refreshSuccess) {
+        const originalRequest = error.config;
+        originalRequest.headers.Authorization = `Bearer ${localStorage.getItem(
+          "accessToken"
+        )}`;
+        return client.request(originalRequest);
+      }
+      window.location.href = "/login";
+    } else return error.response;
+  }
 );
 
 export default client;
--- toky-front/src/lib/withAuth.tsx
+++ toky-front/src/lib/withAuth.tsx
@@ -5,49 +5,53 @@ import { refresh, setTokenFromCookie } from "./httpClient";
 type status = "checking" | "refreshing" | "done";
 
 export default function withAuth<P extends object>(
-	WrappedComponent: React.ComponentType<P>
+  WrappedComponent: React.ComponentType<P>
 ): React.FC<P> {
-	return function ReactComponent(props: P) {
-		const [isAuthed, setIsAuthed] = useState<boolean>(false);
-		const [authStatus, setAuthStatus] = useState<status>("checking");
+  return function ReactComponent(props: P) {
+    const [isAuthed, setIsAuthed] = useState<boolean>(false);
+    const [authStatus, setAuthStatus] = useState<status>("checking");
 
-		const checkAuth = async () => {
-			const refreshToken = localStorage.getItem("refreshToken");
-			if (refreshToken) {
-				setAuthStatus("refreshing");
+    const checkAuth = async () => {
+      const refreshToken = localStorage.getItem("refreshToken");
+      if (refreshToken) {
+        setAuthStatus("refreshing");
 
-				const refreshResult = await refresh();
-				setIsAuthed(refreshResult);
-				setAuthStatus("done");
-			} else {
-				const hasCookie = setTokenFromCookie();
-				setIsAuthed(hasCookie);
-				setAuthStatus("done");
-			}
-		};
-		useEffect(() => {
-			const accessToken = localStorage.getItem("accessToken");
-			if (accessToken) {
-				setIsAuthed(true);
-				setAuthStatus("done");
-			} else checkAuth();
-		}, []);
+        const refreshResult = await refresh();
+        setIsAuthed(refreshResult);
+        setAuthStatus("done");
+      } else {
+        const hasCookie = setTokenFromCookie();
+        setIsAuthed(hasCookie);
+        setAuthStatus("done");
+      }
+    };
+    useEffect(() => {
+      const accessToken = localStorage.getItem("accessToken");
+      if (accessToken) {
+        setIsAuthed(true);
+        setAuthStatus("done");
+      } else checkAuth();
+    }, []);
 
-		// TODO: add loading spinner
-		switch (authStatus) {
-			case "checking":
-			case "refreshing":
-				return <div>refreshing..</div>;
-			case "done":
-				return isAuthed ? <WrappedComponent {...props} /> : <RedirectionComponent />;
-		}
-	};
+    // TODO: add loading spinner
+    switch (authStatus) {
+      case "checking":
+      case "refreshing":
+        return <div>refreshing..</div>;
+      case "done":
+        return isAuthed ? (
+          <WrappedComponent {...props} />
+        ) : (
+          <RedirectionComponent />
+        );
+    }
+  };
 }
 
 const RedirectionComponent = () => {
-	const router = useRouter();
-	useEffect(() => {
-		router.replace("/login");
-	}, []);
-	return null;
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/login");
+  }, []);
+  return null;
 };
