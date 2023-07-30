3adec12f46be7749413ffd94f6329d21d1113735
overthestream <njhnjh02@naver.com>
2023-07-31 00:28:20 +0900

    fix: navbar

--- toky-front/src/components/common/NavigationBar.tsx
+++ toky-front/src/components/common/NavigationBar.tsx
@@ -7,28 +7,37 @@ import Logo from "../../../public/image/logo.svg";
 import SideBar from "./SideBar";
 
 export default function NavigationBar() {
-  const pathname = usePathname();
-  const router = useRouter();
+	const pathname = usePathname();
+	const router = useRouter();
 
 	return (
 		<Wrapper>
-			<Image src={Logo} alt='logo' style={{cursor:"pointer"}} width={52} onClick={()=>{router.push('/')}} />
+			<Image
+				src={Logo}
+				alt="logo"
+				style={{ cursor: "pointer" }}
+				width={52}
+				onClick={() => {
+					router.push("/");
+				}}
+			/>
 			<NavigationWrapper>
-				<div style={{ display: 'flex', gap: 23 }}>
+				<div style={{ display: "flex", gap: 23 }}>
 					<NavigationItem
-						selected={pathname === '/analysis'}
+						selected={pathname === "/analysis"}
 						onClick={() => {
-							router.push('/analysis');
+							router.push("/analysis");
 						}}
 					>
 						전력분석
 					</NavigationItem>
-					<NavigationItem selected={pathname === '/bets'} onClick={() => router.push('/bets')}>
+
+					<NavigationItem selected={pathname === "/bets"} onClick={() => router.push("/bets")}>
 						승부예측
 					</NavigationItem>
 					<NavigationItem
-						selected={pathname === '/ranking'}
-						onClick={() => router.push('/ranking')}
+						selected={pathname === "/ranking"}
+						onClick={() => router.push("/ranking")}
 					>
 						랭킹
 					</NavigationItem>
@@ -37,37 +46,37 @@ export default function NavigationBar() {
 			</NavigationWrapper>
 		</Wrapper>
 	);
-
 }
 
 const Wrapper = styled.div`
-  position: absolute;
-  top: 0px;
-  width: 100%;
-  height: 46px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0px 23px;
-  background-color: #121212;
-  border-bottom: 1px solid rgba(255, 255, 255, 0.15);
-  z-index: 10;
+	position: absolute;
+	top: 0px;
+	width: 100%;
+	height: 46px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 0px 23px;
+	background-color: #121212;
+	border-bottom: 1px solid rgba(255, 255, 255, 0.15);
+	z-index: 10;
 `;
 
 const NavigationWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 25px;
+	display: flex;
+	align-items: center;
+	gap: 25px;
 `;
 
 const NavigationItem = styled.div<{ selected: boolean }>`
 	display: flex;
 	align-items: center;
 	height: 46px;
+
 	font-weight: 400;
 	color: rgba(255, 255, 255, 0.38);
 	cursor: pointer;
 
-  ${({ selected }) =>
-    selected && "color: #ffffff; border-bottom: 3px solid #ffffff;"}
+	${({ selected }) =>
+		selected && "color: #ffffff; border-bottom: 3px solid #ffffff; margin-top: 1.5px"}
 `;
