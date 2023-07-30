1b8a9abb70768f3ff2541e84df1b936c1efa6b4d
overthestream <njhnjh02@naver.com>
2023-07-31 00:45:29 +0900

    refactor

--- toky-front/src/app/bets/page.tsx
+++ toky-front/src/app/bets/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import BetBanner from "@/components/bets/BetBanner";
-import BetTopBar from "@/components/bets/BetTopBar";
 import MatchNavBar from "@/components/bets/MatchNavBar";
 import QuestionList from "@/components/bets/QuestionList";
 import AuthContext from "@/components/common/AuthContext";
@@ -9,92 +8,87 @@ import SharePrediction from "@/components/share/SharePrediction";
 import client from "@/lib/httpClient";
 import withAuth from "@/lib/withAuth";
 import axios from "axios";
-import { use, useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
-import { styled } from "styled-components";
 
 export type QuestionType = {
-  id: number;
-  match: number;
-  description: string;
-  choice: string[];
+	id: number;
+	match: number;
+	description: string;
+	choice: string[];
 };
 
 function Bets() {
-  // useeffect로 axios쏘고 이미 있는지 체크
-  // useffect로 question 받아와서 question set해주기
-  // 서버로 유지 위해 use client를 question list로?
+	// useeffect로 axios쏘고 이미 있는지 체크
+	// useffect로 question 받아와서 question set해주기
+	// 서버로 유지 위해 use client를 question list로?
 
-  const [showModal, setShowModal] = useState(false);
-  const [portalElement, setProtalElement] = useState<Element | null>(null);
-  useEffect(() => {
-    setProtalElement(document.getElementById("portal"));
-  }, [showModal]);
-  function clickModal() {
-    setShowModal(!showModal);
-  }
+	const [showModal, setShowModal] = useState(false);
+	const [portalElement, setProtalElement] = useState<Element | null>(null);
+	useEffect(() => {
+		setProtalElement(document.getElementById("portal"));
+	}, [showModal]);
+	function clickModal() {
+		setShowModal(!showModal);
+	}
 
-  const [match, setMatch] = useState<number>(0);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [match, setMatch] = useState<number>(0);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const [questions, setQuestions] = useState<QuestionType[]>([]);
+	const [questions, setQuestions] = useState<QuestionType[]>([]);
 
-  const handleMatch = (m: number) => {
-    setMatch(m);
-  };
-  const authCtx = useContext(AuthContext);
+	const handleMatch = (m: number) => {
+		setMatch(m);
+	};
+	const authCtx = useContext(AuthContext);
 
-  // 최초 로드 및 match 변경 시 -> 질문 가져오기
-  useEffect(() => {
-    if (authCtx.id === "") {
-      client
-        .get("/auth/profile")
-        .then((res) => {
-          return res.data;
-        })
-        .then((user) => {
-          authCtx.setUserId(user.id);
-          authCtx.setNickname(user.name);
-          const uni = user.university === 0 ? "고려대학교" : "연세대학교";
-          authCtx.setUniv(uni);
-        });
-    }
-    console.log(authCtx);
-  }, []);
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_API_BETS_QUESTIONS) {
-      setIsLoading(true);
+	// 최초 로드 및 match 변경 시 -> 질문 가져오기
+	useEffect(() => {
+		if (authCtx.nickname === "") {
+			client
+				.get("/auth/profile")
+				.then((res) => res.data)
+				.then((user) => {
+					authCtx.setNickname(user.name);
+					const uni = user.university === 0 ? "고려대학교" : "연세대학교";
+					authCtx.setUniv(uni);
+				})
+				.catch((err) => {
+					window.location.href = "/login";
+				});
+		}
+	}, []);
+	useEffect(() => {
+		if (process.env.NEXT_PUBLIC_API_BETS_QUESTIONS) {
+			setIsLoading(true);
 
-      axios
-        .get(`${process.env.NEXT_PUBLIC_API_BETS_QUESTIONS}/${match}`)
-        .then((res) => {
-          if (res.status === 200) {
-            console.log(res.data);
-            // setQuestions(JSON.parse(res.data));
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
-    }
-  }, [match]);
-  return (
-    <>
-      <NavigationBar />
-      <BetBanner match={match} clickModal={clickModal} />
-      <MatchNavBar match={match} handleMatch={handleMatch} />
-      {!isLoading && <QuestionList match={match} questions={questions} />}
-      {showModal && portalElement
-        ? createPortal(
-            <SharePrediction clickModal={clickModal} />,
-            portalElement
-          )
-        : null}
-    </>
-  );
+			axios
+				.get(`${process.env.NEXT_PUBLIC_API_BETS_QUESTIONS}/${match}`)
+				.then((res) => {
+					if (res.status === 200) {
+						console.log(res.data);
+						// setQuestions(JSON.parse(res.data));
+					}
+				})
+				.catch((error) => {
+					console.log(error);
+				})
+				.finally(() => {
+					setIsLoading(false);
+				});
+		}
+	}, [match]);
+	return (
+		<>
+			<NavigationBar />
+			<BetBanner match={match} clickModal={clickModal} />
+			<MatchNavBar match={match} handleMatch={handleMatch} />
+			{!isLoading && <QuestionList match={match} questions={questions} />}
+			{showModal && portalElement
+				? createPortal(<SharePrediction clickModal={clickModal} />, portalElement)
+				: null}
+		</>
+	);
 }
 
 export default withAuth(Bets);
--- toky-front/src/components/common/AuthContext.tsx
+++ toky-front/src/components/common/AuthContext.tsx
@@ -2,40 +2,32 @@
 import React, { ReactNode, useState } from "react";
 
 const AuthContext = React.createContext({
-  id: "",
-  university: "",
-  nickname: "",
-  setUserId: (id: string) => {},
-  setUniv: (univ: string) => {},
-  setNickname: (nickname: string) => {},
+	university: "",
+	nickname: "",
+
+	setUniv: (univ: string) => {},
+	setNickname: (nickname: string) => {},
 });
 type LayoutProps = { children?: ReactNode };
 
 export const AuthContextProvider = ({ children }: LayoutProps) => {
-  const [userId, setUserId] = useState<string>("");
-  const [univ, setUniv] = useState<string>("");
-  const [nickname, setNickname] = useState<string>("");
-  const setUserIdHandler = (id: string) => {
-    setUserId(id);
-  };
-  const setUnivHandler = (univ: string) => {
-    setUniv(univ);
-  };
-  const setNicknameHandler = (nickname: string) => {
-    setNickname(nickname);
-  };
-  const contextValue = {
-    id: userId,
-    university: univ,
-    nickname: nickname,
-    setUserId: setUserIdHandler,
-    setUniv: setUnivHandler,
-    setNickname: setNicknameHandler,
-  };
+	const [univ, setUniv] = useState<string>("");
+	const [nickname, setNickname] = useState<string>("");
+	const setUnivHandler = (univ: string) => {
+		setUniv(univ);
+	};
+	const setNicknameHandler = (nickname: string) => {
+		setNickname(nickname);
+	};
+	const contextValue = {
+		university: univ,
+		nickname: nickname,
 
-  return (
-    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
-  );
+		setUniv: setUnivHandler,
+		setNickname: setNicknameHandler,
+	};
+
+	return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContext;
