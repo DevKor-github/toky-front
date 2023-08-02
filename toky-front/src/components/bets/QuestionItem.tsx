a7802a5e2197c0ba2f3b35510a87108ece89d1be
SeokWon <tjrdnjs1534@naver.com>
2023-08-02 22:04:59 +0900

    fix: 3개 디자인 수정

--- toky-front/src/components/bets/QuestionItem.tsx
+++ toky-front/src/components/bets/QuestionItem.tsx
@@ -194,7 +194,7 @@ const AnswerBtn = styled.button<{
           return props.selected === 0
             ? `background: linear-gradient(90deg, #f3233c 0%, rgba(243, 35, 60, 0.25) 100%);
         box-shadow: 4px 0px 4px rgba(0, 0, 0, 0.25); border-radius: 7px 0px 0px 7px; color : white`
-            : "color: rgba(255, 255, 255, 0.15); background: #181818";
+            : "color: rgba(255, 255, 255, 0.15); background: #181818; border-radius: 7px 0px 0px 7px;";
         } else if (props.index === 1) {
           return props.selected === 1
             ? `background: linear-gradient(90deg, rgba(76, 14, 176, 0.6) -12.75%, #4C0EB0 15.63%, #4C0EB0 60.71%, rgba(76, 14, 176, 0.6) 113.73%);
@@ -204,7 +204,7 @@ box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25)`
           return props.selected === 2
             ? `background: linear-gradient(90deg, rgba(41, 72, 255, 0.25) 0%, #2948FF 100%); 
         box-shadow: -4px 0px 4px rgba(0, 0, 0, 0.25); border-radius: 0px 7px 7px 0px; color : white`
-            : "color: rgba(255, 255, 255, 0.15); background: #181818";
+            : "color: rgba(255, 255, 255, 0.15); background: #181818; border-radius: 0px 7px 7px 0px;";
         }
       }
     }
