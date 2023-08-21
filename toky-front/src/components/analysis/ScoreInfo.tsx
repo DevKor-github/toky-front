bf6faa82ca8f0903b8940a5522e561cf7ab54592
SeokWon <tjrdnjs1534@naver.com>
2023-08-21 14:18:58 +0900

    feat: past match 구현

--- toky-front/src/components/analysis/ScoreInfo.tsx
+++ toky-front/src/components/analysis/ScoreInfo.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Image from "next/image";
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 import CharImage from "../../../public/image/Logo.webp";
 import { MatchProps } from "./MatchType";
 import { pastMatch } from "./Data";
 import React from "react";
+import KoreaLogo from "../../../public/image/PastMatchKoreaLogo.png";
+import YonseiLogo from "../../../public/image/PastMatchYonseiLogo.png";
 export default function ScoreInfo({ match }: MatchProps) {
   //10경기 -- 한 종목당 2개
   //match 받아서
@@ -25,12 +27,32 @@ export default function ScoreInfo({ match }: MatchProps) {
       </ScoreInfowrapper>
       {pastMatch[match].map((c, i) => (
         <React.Fragment key={i}>
-          <MatchContainer>
+          <MatchContainer $result={c.result}>
+            {i % 2 == 0 && <p className="univ">KOREA</p>}
             <MatchTextContainer>
               <h1>{c.score}</h1>
               <h2>{c.date}</h2>
               <h4>{c.leagueName}</h4>
             </MatchTextContainer>
+            {i % 2 !== 0 && (
+              <>
+                <Image
+                  src={KoreaLogo}
+                  alt="korea logo"
+                  width={90}
+                  height={105}
+                  style={{ position: "absolute", bottom: "0", left: "10px" }}
+                />
+                <Image
+                  src={YonseiLogo}
+                  alt="yonsei logo"
+                  width={118}
+                  height={111}
+                  style={{ position: "absolute", bottom: "0", right: "8px" }}
+                />
+              </>
+            )}
+            {i % 2 == 0 && <p className="univ">YONSEI</p>}
           </MatchContainer>
         </React.Fragment>
       ))}
@@ -74,27 +96,63 @@ const CharImageWrapper = styled.div`
   transform: translate(-50%, -50%);
 `;
 
-const MatchContainer = styled.div`
-  opacity: 0.5;
-  background: linear-gradient(270deg, #4c0eb0 0%, rgba(76, 14, 176, 0.1) 100%);
+const MatchContainer = styled.div<{ $result: number }>`
+  ${(props) => {
+    if (props.$result === 0) {
+      return css`
+        background: linear-gradient(
+          90deg,
+          #f3233c,
+          rgba(243, 35, 60, 0.1) 50%,
+          rgba(41, 72, 255, 0.3) 100%
+        );
+      `;
+    } else {
+      return css`
+        background: linear-gradient(
+          90deg,
+          rgba(243, 35, 60, 0.3),
+          rgba(243, 35, 60, 0.03) 50%,
+          #2948ff 100%
+        );
+      `;
+    }
+  }}
   min-height: 138px;
   display: flex;
-  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  position: relative;
+
+  .univ {
+    opacity: 0.2;
+    color: #fff;
+    font-family: Inter;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
 `;
 const MatchTextContainer = styled.div`
   color: white;
   text-align: center;
+  min-width: 178px;
   h1 {
     font-size: 36px;
     font-weight: 700;
+    margin-bottom: 13px;
+    white-space: nowrap;
   }
   h2 {
     font-size: 13px;
     font-weight: 700;
+    margin-bottom: 5px;
+    white-space: nowrap;
   }
   h4 {
     font-size: 8px;
     font-weight: 500;
+    white-space: nowrap;
   }
 `;
