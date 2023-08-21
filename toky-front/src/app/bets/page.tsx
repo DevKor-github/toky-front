efacfd183c5e9b95bf6dd92cf3c78afa9b14cf86
overthestream <njhnjh02@naver.com>
2023-08-21 15:18:41 +0900

    remove naver oauth

--- toky-front/src/app/bets/page.tsx
+++ toky-front/src/app/bets/page.tsx
@@ -17,123 +17,106 @@ import ModalPortal from "@/components/common/ModalPortal";
 import BetFailModal from "@/components/bets/BetFailModal";
 
 export interface QuestionType {
-  questionId: number;
-  description: string;
-  choices: string[];
-  percentage: number[];
-  answer: number | null;
+	questionId: number;
+	description: string;
+	choices: string[];
+	percentage: number[];
+	answer: number | null;
 }
 
 function Bets() {
-  // useeffect로 axios쏘고 이미 있는지 체크
-  // useffect로 question 받아와서 question set해주기
-  // 서버로 유지 위해 use client를 question list로?
+	// useeffect로 axios쏘고 이미 있는지 체크
+	// useffect로 question 받아와서 question set해주기
+	// 서버로 유지 위해 use client를 question list로?
 
-  const [showShareModal, setShowShareModal] = useState(false);
-  const [showPointModal, setShowPointModal] = useState(false);
-  const [showWaitModal, setShowWaitModal] = useState(false);
-  const [showFailModal, setShowFailModal] = useState(false);
-  function clickShareModal() {
-    setShowShareModal(!showShareModal);
-  }
-  function autoPointModal() {
-    setShowPointModal(true);
-    setTimeout(() => {
-      setShowPointModal(false);
-    }, 1000);
-  }
-  function clickPointModal() {
-    setShowPointModal(false);
-  }
-  function clickFailModal() {
-    setShowFailModal(!showFailModal);
-  }
-  function clickWaitModal() {
-    setShowWaitModal(!showWaitModal);
-  }
+	const [showShareModal, setShowShareModal] = useState(false);
+	const [portalElement, setProtalElement] = useState<Element | null>(null);
+	const [showPointModal, setShowPointModal] = useState(false);
 
-  const [match, setMatch] = useState<number>(0);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [questions, setQuestions] = useState<QuestionType[]>([]);
+	useEffect(() => {
+		setProtalElement(document.getElementById("portal"));
+	}, [showShareModal]);
 
-  const handleMatch = (m: number) => {
-    setMatch(m);
-  };
+	function clickShareModal() {
+		setShowShareModal(!showShareModal);
+	}
+	function clickPointModal() {
+		setShowPointModal(!showPointModal);
+	}
 
-  const authCtx = useContext(AuthContext);
-  const [matchProgress, setMatchProgress] = useState<boolean>(false);
+	const [match, setMatch] = useState<number>(0);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  // 최초 로드 및 match 변경 시 -> 질문 가져오기
-  useEffect(() => {
-    if (authCtx.nickname === "") {
-      client
-        .get("/auth/profile")
-        .then((res) => res.data)
-        .then((user) => {
-          authCtx.setNickname(user.name);
-          const uni = user.university === 0 ? "고려대학교" : "연세대학교";
-          authCtx.setUniv(uni);
-          authCtx.setScore(user.score);
-          authCtx.setRemain(user.remain);
-          authCtx.setPhoneNum(user.phoneNumber);
-        })
-        .catch((err) => {
-          window.location.href = "/login";
-        });
-    }
-    client
-      .get("/bets/questions")
-      .then((res) => res.data)
-      .then((data) => {
-        return data;
-      })
-      .then((data) => setQuestions(data))
-      .finally(() => setIsLoading(false));
-  }, []);
-  useEffect(() => {
-    setMatchProgress(ProgressCheck(TIME[match]));
-  }, [match]);
-  const questionsInMatch = questions
-    .sort((a, b) => a.questionId - b.questionId)
-    .filter((q) => q.questionId > match * 5 && q.questionId <= match * 5 + 5);
+	const [questions, setQuestions] = useState<QuestionType[]>([]);
 
-  return (
-    <>
-      <NavigationBar />
-      <PageTransitionWrapper>
-        <BetBanner
-          match={match}
-          matchProgress={matchProgress}
-          clickModal={clickShareModal}
-        />
-        <MatchNavBar match={match} handleMatch={handleMatch} />
-        {!isLoading && (
-          <QuestionList
-            questions={questionsInMatch}
-            setQuestions={setQuestions}
-            orgQuestions={questions}
-            setPointModal={autoPointModal}
-            setWaitModal={clickWaitModal}
-            setFailModal={clickFailModal}
-            match={match}
-            matchProgress={matchProgress}
-          />
-        )}
-        <ModalPortal isShowing={showPointModal}>
-          <PointModal />
-        </ModalPortal>
-        <ModalPortal isShowing={showShareModal}>
-          <SharePrediction clickModal={clickShareModal} />
-        </ModalPortal>
-        <ModalPortal isShowing={showWaitModal}>
-          <BetWaitModal clickModal={clickWaitModal} />
-        </ModalPortal>
-        <ModalPortal isShowing={showFailModal}>
-          <BetFailModal clickModal={clickFailModal} />
-        </ModalPortal>
-      </PageTransitionWrapper>
-    </>
-  );
+	const handleMatch = (m: number) => {
+		setMatch(m);
+	};
+
+	const authCtx = useContext(AuthContext);
+	const [matchProgress, setMatchProgress] = useState<boolean>(false);
+
+	// 최초 로드 및 match 변경 시 -> 질문 가져오기
+	useEffect(() => {
+		if (authCtx.nickname === "") {
+			client
+				.get("/auth/profile")
+				.then((res) => res.data)
+				.then((user) => {
+					authCtx.setNickname(user.name);
+					const uni = user.university === 0 ? "고려대학교" : "연세대학교";
+					authCtx.setUniv(uni);
+
+					authCtx.setScore(user.score);
+					authCtx.setRemain(user.remain);
+				})
+				.catch((err) => {
+					window.location.href = "/login";
+				});
+		}
+		client
+			.get("/bets/questions")
+			.then((res) => res.data)
+			.then((data) => {
+				return data;
+			})
+			.then((data) => setQuestions(data))
+			.finally(() => setIsLoading(false));
+	}, []);
+	useEffect(() => {
+		setMatchProgress(ProgressCheck(TIME[match]));
+	}, [match]);
+	const questionsInMatch = questions
+		.sort((a, b) => a.questionId - b.questionId)
+		.filter((q) => q.questionId > match * 5 && q.questionId <= match * 5 + 5);
+
+	return (
+		<>
+			<NavigationBar />
+			<BetBanner match={match} matchProgress={matchProgress} clickModal={clickShareModal} />
+			<MatchNavBar match={match} handleMatch={handleMatch} />
+			{!isLoading && (
+				<QuestionList
+					questions={questionsInMatch}
+					setQuestions={setQuestions}
+					orgQuestions={questions}
+					setModal={clickPointModal}
+					match={match}
+					matchProgress={matchProgress}
+				/>
+			)}
+			{showPointModal && portalElement
+				? createPortal(
+						// <SharePrediction clickModal={clickShareModal} />
+						<PointModal clickModal={clickPointModal} />,
+						portalElement
+				  )
+				: null}
+			{showShareModal && portalElement
+				? createPortal(<SharePrediction clickModal={clickShareModal} />, portalElement)
+				: null}
+		</>
+	);
 }
 
 export default withAuth(Bets);
--- toky-front/src/components/login/loginComponent.tsx
+++ toky-front/src/components/login/loginComponent.tsx
@@ -5,103 +5,83 @@ import Image from "next/image";
 import toky_bg from "../../../public/image/toky_login_img.png";
 import Logo from "../../../public/image/logo.svg";
 import { easeIn, motion } from "framer-motion";
+import kakaoSymbol from "../../../public/image/kakaoSymbol.png";
 
 export default function LoginComponent() {
-  const kakaoLogin = async () => {
-    window.location.href = process.env.NEXT_PUBLIC_API_URL + "/auth/kakao";
-  };
+	const kakaoLogin = async () => {
+		window.location.href = process.env.NEXT_PUBLIC_API_URL + "/auth/kakao";
+	};
 
-  const naverLogin = async () => {
-    window.location.href = process.env.NEXT_PUBLIC_API_URL + "/auth/naver";
-  };
-
-  return (
-    <LoginContainer
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5, ease: easeIn }}
-    >
-      <Image
-        className="toky_logo"
-        src={Logo}
-        width={239}
-        height={78}
-        alt="logo"
-      />
-      <Image
-        width={390}
-        height={468}
-        className="toky_bg"
-        priority={true}
-        src={toky_bg}
-        alt="bg"
-      />
-      <LoginBtnContainer>
-        <LoginBtn className="kakao" onClick={kakaoLogin}>
-          카카오톡으로 시작하기
-        </LoginBtn>
-        <LoginBtn className="naver" onClick={naverLogin}>
-          네이버로 시작하기
-        </LoginBtn>
-      </LoginBtnContainer>
-    </LoginContainer>
-  );
+	return (
+		<LoginContainer
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 0.5, ease: easeIn }}
+		>
+			<Image className="toky_logo" src={Logo} width={239} height={78} alt="logo" />
+			<Image width={390} height={468} className="toky_bg" priority={true} src={toky_bg} alt="bg" />
+			<LoginBtnContainer>
+				<LoginBtn className="kakao" onClick={kakaoLogin}>
+					<Image src={kakaoSymbol} width={30} height={30} />
+					카카오톡으로 시작하기
+				</LoginBtn>
+			</LoginBtnContainer>
+		</LoginContainer>
+	);
 }
 
 const LoginContainer = styled(motion.div)`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: flex-end;
-  height: 100vh;
-  position: relative;
-  & .toky_logo {
-    position: absolute;
-    top: 15%;
-    left: 50%;
-    transform: translate(-50%, -30%);
-    z-index: 3;
-  }
-  & .toky_bg {
-    vertical-align: bottom;
-  }
-  padding-bottom: 7vh;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: flex-end;
+	height: 100vh;
+	position: relative;
+	& .toky_logo {
+		position: absolute;
+		top: 15%;
+		left: 50%;
+		transform: translate(-50%, -30%);
+		z-index: 3;
+	}
+	& .toky_bg {
+		vertical-align: bottom;
+	}
+	padding-bottom: 7vh;
 `;
 
 const LoginBtnContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
 `;
 const LoginBtn = styled.button`
-  width: 90%;
-  height: 46px;
-  text-align: center;
-  margin-bottom: 8px;
-  margin-left: auto;
-  margin-right: auto;
-  cursor: pointer;
-  border-radius: 4px;
-  color: #121212;
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 20px;
-  letter-spacing: -0.06em;
-  border: none;
-  /* transform: translateY(-25px); */
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
+	width: 90%;
+	height: 46px;
+	text-align: center;
+	margin-bottom: 8px;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	cursor: pointer;
+	border-radius: 12px;
+	color: rgba(0, 0, 0, 0.85);
+	font-style: normal;
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 20px;
+	letter-spacing: -0.06em;
+	border: none;
+	/* transform: translateY(-25px); */
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
 
-  &.kakao {
-    background: #ffbe16;
-  }
-
-  &.naver {
-    background: #13e059;
-  }
+	&.kakao {
+		background: #fee500;
+	}
 `;
