97e8989048ce35f1f80bcc37ecf8388093d6ca4d
overthestream <njhnjh02@naver.com>
2023-08-27 23:07:23 +0900

    add info

--- toky-front/src/components/home/MainFooter.tsx
+++ toky-front/src/components/home/MainFooter.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 import Image from "next/image";
 import { Space } from "../common/Space";
 import devKor from "../../../public/image/devkor_logo.png";
@@ -8,8 +8,29 @@ import aws from "../../../public/image/aws_logo.png";
 import kuaa from "../../../public/image/kuaa_logo.png";
 import sportsku from "../../../public/image/sportsku_logo.png";
 import sisboombah from "../../../public/image/sisboombah_logo.png";
+import sportskuCover from "../../../public/image/sportsku_cover.png";
+import { RefObject, useEffect, useRef, useState } from "react";
 
 export default function MainFooter() {
+  const ref = useRef<HTMLImageElement | null>(null);
+  const [isKuSportsOpen, setIsKuSportsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setIsKuSportsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const handleKuSportsOpen = () => {
+    setIsKuSportsOpen(!isKuSportsOpen);
+  };
+
   return (
     <MainFooterWrapper>
       <Flex>
@@ -29,14 +50,31 @@ export default function MainFooter() {
       <MaterialProvisionInfo>
         <Text>자료제공</Text>
         <ImageFlex>
-          <Image alt="sportku" width={91} src={sportsku} />
+          <Image
+            alt="sportku"
+            width={91}
+            src={sportsku}
+            onClick={handleKuSportsOpen}
+            ref={ref as RefObject<HTMLImageElement>}
+          />
           <Image alt="sisboombah" width={106} src={sisboombah} />
         </ImageFlex>
+        <KuSportsInfo visible={isKuSportsOpen}>
+          <Image alt="sports ku cover" src={sportskuCover} width={300} />
+          <div>앞서가는 대학 스포츠 문화 월간지 SPORTS KU!</div>
+          <div>다양한 방식으로 SPORTS KU를 만나보세요.</div>
+          <div>메일 | sportsku@naver.com</div>
+          <div>블로그 | blog.naver.com/sportsku</div>
+          <div>유튜브 | youtube.com/user/sp0rtsku</div>
+          <div>인스타그램 | @sports_ku</div>
+          <div>1년 정기구독 30,000원 (배송료 포함, 입금 확인 후 발송)</div>
+          <div>송금 계좌번호 | 카카오뱅크 3333277099225 (예금주 문상은)</div>
+          <div>송금 후 이름, 주소, 연락처를 sportsku@naver.com 으로 보내주시기 바랍니다</div>
+          <div>문의전화 | 010-2170-4757 (부국장 문상은)</div>
+        </KuSportsInfo>
       </MaterialProvisionInfo>
       <Space h={48} />
-      <Copyright>
-        Copyright ⓒ 2023 정기전 승부예측 토키. All Rights Reserved.
-      </Copyright>
+      <Copyright>Copyright ⓒ 2023 정기전 승부예측 토키. All Rights Reserved.</Copyright>
     </MainFooterWrapper>
   );
 }
@@ -96,4 +134,31 @@ const ImageFlex = styled.div`
   display: flex;
   align-items: center;
   gap: 20px;
+`;
+
+const KuSportsInfo = styled.div<{ visible: boolean }>`
+  background-color: rgba(255, 255, 255, 0.9);
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: start;
+  gap: 7px;
+  width: 320px;
+  top: 100px;
+  padding: 10px;
+  z-index: 3;
+  border-radius: 10px;
+  ${(props) =>
+    !props.visible &&
+    css`
+      display: none;
+    `};
+  div {
+    font-family: Spoqa Han Sans Neo;
+    font-size: 12px;
+    font-weight: 400;
+    letter-spacing: -0.4px;
+    width: 100%;
+  }
 `;
